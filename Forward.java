ca3cedadecae3e5b5e7592da0d820d730446b464
JackHuhs <72474445+JackHuhs@users.noreply.github.com>
2020-11-11 13:44:23 -0800

    Update Forward.java

--- Forward.java
+++ Forward.java
@@ -33,6 +33,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
@@ -60,6 +61,7 @@ public class Forward extends LinearOpMode {
     DcMotor rightDrive;
     DcMotor leftDownDrive;
     DcMotor rightDownDrive;
+    Servo launcherPush;
     double power = 0.5;
 
     @Override
